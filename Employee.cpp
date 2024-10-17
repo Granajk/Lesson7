cc28f6486481c0374c552a27c427d6893590f4f3
Rodion <roksis72010@gmail.com>
2024-10-17 15:47:30 +0200

    Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -2,35 +2,45 @@
 
 Employee::Employee()
 {
+    name = "undefined";
+    salary = 7000.0f;
 }
 
 Employee::Employee(string name, float salary)
 {
+    this->name = name;
+    this->salary = salary;
 }
 
 string Employee::getName() const
 {
-    return string();
+    return name;
 }
 
 float Employee::getSalary() const
 {
-    return 0.0f;
+    return salary;
 }
 
 void Employee::setName(string name)
 {
+
+    this->name = name;
 }
 
 void Employee::setSalary(float salary)
 {
+    this->salary = salary;
 }
 
 float Employee::calcSalary() const
 {
-    return 0.0f;
+    return salary * 0.805f;
 }
 
 void Employee::showInfo() const
 {
+    cout << "________________________\n";
+    cout << "Name: " << name << endl;
+    cout << "Salary: " << salary << endl;
 }
