c1b06b2b03fae72196450c3df198846c7be5a695
Rodion <roksis72010@gmail.com>
2024-10-17 15:44:36 +0200

    Employee

--- Employee.h
+++ Employee.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
 class Employee
 {
+protected:
+	string name;
+	float salary;
+public:
+	Employee();
+	Employee(string name,
+		float salary);
+
+	string getName()const;
+	float getSalary()const;
+
+	void setName(string name);
+	void setSalary(float salary);
+
+	float calcSalary()const;
+
+	void showInfo()const;
+
 };
 
